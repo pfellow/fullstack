2f802da8832d24b4d22ae39ff3edb9f544fee2f0
pfellow <pfellow@gmail.com>
2023-06-02 00:02:02 +1200

    added db

--- index-ws.js
+++ index-ws.js
@@ -13,10 +13,46 @@ server.listen(PORT, function () {
   console.log('Listening on ' + PORT);
 });
 
+/** Begin database **/
+
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `);
+});
+
+const getCounts = () => {
+  db.each('SELECT * FROM visitors', (err, row) => {
+    console.log(row);
+  });
+};
+
+const shutdownDB = () => {
+  getCounts();
+  console.log('Shutting down db');
+  db.close();
+};
+/** End database **/
+
 /** Websocket **/
 const WebSocketServer = require('ws').Server;
 
 const wss = new WebSocketServer({ server: server });
+
+process.on('SIGINT', () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
 
 wss.on('connection', function connection(ws) {
   const numClients = wss.clients.size;
@@ -29,6 +65,10 @@ wss.on('connection', function connection(ws) {
   if (ws.readyState === ws.OPEN) {
     ws.send('welcome!');
   }
+
+  db.run(
+    `INSERT INTO visitors (count, time) VALUES (${numClients}, datetime('now'))`
+  );
 
   ws.on('close', function close() {
     wss.broadcast(`Current visitors: ${wss.clients.size}`);
